8da9d80dd049b80456875b8e435815417561b3ab
Nick Morgan <nick@nicholas-morgan.com>
2015-10-09 13:19:13 -0700

    changed default bower install directory

--- js/controllers/my-ctrl-1.js
+++ js/controllers/my-ctrl-1.js
@@ -1,4 +1,8 @@
 define(['./module'], function (controllers) {
     'use strict';
-    controllers.controller('MyCtrl1', [function ($scope) {}]);
+    controllers.controller('MyCtrl1', [function ($scope) {
+
+
+
+    }]);
 });
--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@
 require.config({
 
     paths: {
-        'angular': '../lib/angular/angular',
-        'angular-route': '../lib/angular-route/angular-route',
-        'domReady': '../lib/requirejs-domready/domReady'
+        "angular": "vendor/angular/angular",
+        "angular-route": "vendor/angular-route/angular-route",
+        "domReady": "vendor/requirejs-domready/domReady"
     },
 
     /**
